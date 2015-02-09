88417df9c0eacabbfe307012f012c48af752c7f1
constantino <constantinomora@gmail.com>
2015-02-09 03:55:51 -0600

    fix: there was a bug on processing another image without cleaning up the previous histogram

--- hw1_EdgeDetection/main.py
+++ hw1_EdgeDetection/main.py
@@ -26,7 +26,7 @@ histogram = []
 
 def preProcessImg(imagePath):
     img = cv2.imread(imagePath)
-    img = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+    img = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY) #convert to grayscale
     imgCopy = img
     height, width = img.shape
 
@@ -35,8 +35,8 @@ def preProcessImg(imagePath):
 def loadFile():
     fname = ""
     try:
-        fname = askopenfilename(filetypes=(("JPG", "*.jpg"),
-        ("All files", "*.*")))
+        fname = askopenfilename(filetypes=(("All files", "*.*"),
+        ("PNG", "*.png")))
 
     except ValueError:
         print "Please select an image. Try again..."
@@ -45,35 +45,35 @@ def loadFile():
 
 def applyMasks(pixel,img):
 
-    pixelGradients = []
-    indexer = [-pixel[0]+1,-pixel[1]+1]
+    pixelGradients = [] # used to store the gradients of each pixel 
+    indexer = [-pixel[0]+1,-pixel[1]+1] # used to get the equivalent position in the mask
 
     for i in range(len(mask)):
         gradient = 0
-        for r in range(pixel[0]-1, pixel[0]+2):
+        for r in range(pixel[0]-1, pixel[0]+2): # used for a neighborhood 3x3... I need to improve it
             for c in range(pixel[1]-1,pixel[1]+2):
-                gradient += img[r,c]*mask[i][r+indexer[0]][c+indexer[1]]
+                gradient += img[r,c]*mask[i][r+indexer[0]][c+indexer[1]] #convolution matrix * each pixel
         
-        pixelGradients.append(gradient)
+        pixelGradients.append(gradient) #the gradient of the mask applied is stored
 
-    maxGradient = max(pixelGradients)
+    maxGradient = max(pixelGradients) #the gradient of the pixel is the maximum gradient obtained
 
     return maxGradient
 
 
 def getThreshold(histogram):
-
-    t_new = max(histogram)/2.0
+    #Basic global thresholding
+    t_new = max(histogram)/2.0 #the threshold should be close to the center
     t_old = 0.0
     mean = [0,0]
-    while abs(t_old-t_new) > 0.001:
+    while abs(t_old-t_new) > 0.001: #the loop ends until the difference of the old threshold and the new one is minimum
 
         if debug:
             print "-> threshold: ",t_new
 
-        mean = getAverages(t_new)
+        mean = getAverages(t_new) #get the average of each side based on the current threshold
         t_old = t_new
-        t_new = 0.5*(mean[0]+mean[1])
+        t_new = 0.5*(mean[0]+mean[1]) # get a new threshold by obtaining the average of the means
     
     return t_new
 
@@ -85,14 +85,14 @@ def getAverages(t):
     sum_down = 0
     counter_down = 0
     for e in histogram:
-        if e > t:
+        if e > t: #sum all the gradients > threshold
             sum_upper += e
             counter_upper += 1
-        else:
+        else: #sum all the gradients < threshold
             sum_down += e
             counter_down += 1
 
-    return [(sum_upper/counter_upper*1.0),(sum_down/counter_down*1.0)]
+    return [(sum_upper/counter_upper*1.0),(sum_down/counter_down*1.0)] #return means 
 
 def processImage():
     
@@ -119,7 +119,7 @@ def processImage():
 
     for y in range(1,height-1,1):
         for x in range(1,width-1,1):
-            histogram.append( applyMasks([y,x],img) )
+            histogram.append( applyMasks([y,x],img) ) #Apply masks for each pixel and save the gradient in the histogram
 
     T = getThreshold(histogram)
     
@@ -130,7 +130,7 @@ def processImage():
     for r in range(1,height-1,1):
         for c in range(1,width-1,1):
 
-            if histogram[index_counter] > T:
+            if histogram[index_counter] > T: #Change the color of the borders, borders in white, everything else in black
                 
                 imgCopy[r,c] = 255
             else:
@@ -139,9 +139,12 @@ def processImage():
 
             index_counter += 1
 
-    cv2.imwrite("img/result.png",imgCopy)
+    cv2.imwrite("img/result.png",imgCopy) #Write a new image
 
     print "-> image successfully saved at img/result.png"
+
+    del histogram[:] # remove all the elements in the histogram
+
     print "-> You can choose another image ..."
 
 def main():
