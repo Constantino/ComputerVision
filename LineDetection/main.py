c98159d70207a1b06f49ef3f134d9b4384f5ef08
constantino <constantinomora@gmail.com>
2015-03-02 01:41:32 -0600

    Starting LineDetection

---
+++ LineDetection/main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import cv2
+from Tkinter import *
+from tkFileDialog import *
+import sys
+from numpy import *
+
+def groupByAngle():
+    return
+
+def getFromGroup():
+    return
+
+def calcDistributionOfSizes():
+    return
+
+def discardLittleLines():
+    return
+
+def getEquation():
+    return
+
+def drawLines():
+    return
+
+
