c983c54665750fcb75a91b943eaca581f007c848
constantino <constantinomora@gmail.com>
2015-03-02 02:17:18 -0600

    Grouping by angle

--- LineDetection/main.py
+++ LineDetection/main.py
@@ -5,9 +5,30 @@ from Tkinter import *
 from tkFileDialog import *
 import sys
 from numpy import *
+from EdgeDetection import EdgeDetection as ed
 
-def groupByAngle():
-    return
+test = ed.EdgeDetection()
+test.path = "figures.png"
+borders = test.detectBorders()
+
+#         0  45 90 -45
+groups = [[],[],[],[]]
+
+def groupByAngle(test):
+    
+    for e in test.border:
+        indexCounter = 0
+        for r in range(1,test.height-1,1):
+            for c in range(1,test.width-1,1):
+                
+                if test.angles[indexCounter] == 0:
+                    groups[0].append([r,c])
+                elif test.angles[indexCounter] == 45:
+                    groups[1].append([r,c])
+                elif test.angles[indexCounter] == 90:
+                    groups[2].append([r,c])
+                elif test.angles[indexCounter] == -45:
+                    groups[3].append([r,c])
 
 def getFromGroup():
     return
