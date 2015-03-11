bd0f4ce3714b1f283649c4450b580f7e59e5ab53
constantino <constantinomora@gmail.com>
2015-03-11 01:45:08 -0600

    Main methods added

--- LineDetection/main.py
+++ LineDetection/main.py
@@ -9,28 +9,8 @@ from numpy import *
 from ShapeDetection import ShapeDetection as sd
 import math
 
-def groupByAngle():
-    
-    for e in shapeDetector.test.border:
-        indexCounter = 0
-        for r in range(1,shapeDetector.test.height-1,1):
-            for c in range(1,shapeDetector.test.width-1,1):
-                print shapeDetector.test.angles[indexCounter]
-                
-                if shapeDetector.test.angles[indexCounter] == 0:
-                    groups[0].append([r,c])
-                elif shapeDetector.test.angles[indexCounter] == 45:
-                    groups[1].append([r,c])
-                elif shapeDetector.test.angles[indexCounter] == 90:
-                    groups[2].append([r,c])
-                elif shapeDetector.test.angles[indexCounter] == -45:
-                    groups[3].append([r,c])
-
-                indexCounter += 1
-                
-
-def getFromGroup():
-    detect = shapeDetector.applyDFS(shapeDetector.test,groups[0],background)
+def getFromGroup(shapeDetector):
+    detect = shapeDetector.applyDFS(shapeDetector.test,shapeDetector.test.border,shapeDetector.myBackground)
     return detect
     """
     for i in detect:
@@ -62,7 +42,68 @@ def getEquation(point,angle=0):
 def drawLines():
     return
 
+"""
+Get angle
+Identify the rho for each pixel through the image 
+dataX
+dataY.append(dataX)
+"""
+"""
+def makeVotationByRho(test):
+	container = list()
+	for y in xrange(test.height):
+		xData = list()
+		for x in xrange(test.width):
+			rho = (x-test.width/2)*cos(test.angles[y][x]) + (y/2 - test.height)*sin(test.angles[y][x])
+			xData.append( test.histogram[y][x],(int(180 * (test.angles[y][x] / 3.1416)) / 18), rho )
+		container.append(xData)
+	return container
+"""
+def makeVotationByRho(points,test):
+	container = list()
+	index_counter = 0
+	print "points:", points
+	for y in range(1,test.height-1,1):
+		for x in range(1,test.width-1,1):
+			#print (y,x)
+			if [y,x] in points:
+				print "I am in points"
+				rho = (x-test.width/2)*cos(test.angles[index_counter]) + (y/2 - test.height)*sin(test.angles[index_counter]) 
+				print "rho: ",rho
+				
+				container.append([test.angles[index_counter],'%.0f' % (int(180 * (test.angles[index_counter] / 3.1416))/4), '%.0f' % rho,[y,x]])
+				index_counter += 1
+	return container
 
+def createCombinations(container,test):
+	comb = dict()
+	for e in container:
+		magnitude,angle,rho,point = e
+		combination = (angle,rho)
+		if combination in comb:
+			comb[combination] += 1
+		else:
+			comb[combination] = 1
+	return comb
+
+def countMostFrecuent(comb):
+	freq = dict()
+	bigger = 0
+	for item in comb:
+		if item in freq:
+			freq[item] += 1
+			if freq[item] > freq[bigger]:
+				bigger = item
+		else:
+			if len(freq) == 0:
+				bigger = item
+			freq[item] = 1
+		
+	print "Freq: ",freq
+		
+	return bigger
+
+#starting
 shapeDetector = sd.ShapeDetection()
 shapeDetector.test.path = "LineDetection/figures.png"
 shapeDetector.test.detectBorders()
@@ -72,24 +113,49 @@ background = shapeDetector.getBackground()
 groups = [[],[],[],[]]
 
 def main():
+	fig = getFromGroup(shapeDetector)
+	container = makeVotationByRho(fig,shapeDetector.test)
+	comb = createCombinations(container,shapeDetector.test)
+	biggerComb = countMostFrecuent(comb)
+	print ""
+	print "comb: ",comb
+	print ""
+	print "container: ",container
+	for i in container:
+		#print "entro a container"
+		magnitude,angle,rho,point = i
+		print "point: ",point
+		print "bg: ", biggerComb
+		print "angle,rho ",(angle,rho)
+		comp = (angle,rho)
+		if biggerComb == comp:	
+			print "pinto de rojo"
+			shapeDetector.test.originalImg[point] = [0,0,255]
+			
+
+	print "printing img"
+	cv2.imwrite("lines_Test.png",shapeDetector.test.originalImg)
+	print "it's supposed img is saved"
+
+def x():
     groupByAngle()
 
-    pointsGrouped = getFromGroup()
+    pointsGrouped = getFromGroup(shapeDetector)
 
     for point in pointsGrouped:
-        getEquation(point)
+        
 
-    maxValue = 0
-    keyChosen = ""
+	    maxValue = 0
+	    keyChosen = ""
 
-    print c
+	    print c
 
-    for key,value in c.items():
-        if value > maxValue:
-            maxValue = value
-            keyChosen = key
+	    for key,value in c.items():
+		if value > maxValue:
+		    maxValue = value
+		    keyChosen = key
 
-    print "key: ",key," value: ",value
+	    print "key: ",key," value: ",value
     
 
 main()
