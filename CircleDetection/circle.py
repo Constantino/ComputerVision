86f5a69fee4ec0db954b8e0b84437ea6606b4e3c
constantino <constantinomora@gmail.com>
2015-03-16 20:21:26 -0600

    Drawing equation points

--- CircleDetection/circle.py
+++ CircleDetection/circle.py
@@ -4,6 +4,12 @@ import numpy as np
 import cv2
 from ShapeDetection import ShapeDetection as sd
 
+def circleEquation(x,xc,y,yc,r):
+	c = (x-xc)**2 + (y-yc)**2
+	r2 = r**2
+	# r2-200 <= c <= r2
+	return  c <= r2  and c >= (r2-200)
+
 def applyDGMasks(img,heigh,width,mask,border):
 		print "applying DG mask"
 		Gx = []
@@ -87,7 +93,10 @@ for z in range(y_min,y_max+1):
 
 centerOfMass = [sum_y/n_elem,sum_x/n_elem]
 radio = centerOfMass[1] - x_min
-shapeDetector.test.originalImg[centerOfMass[0],centerOfMass[1]] = [0,255,0]
+
+for y in xrange(centerOfMass[0]-1,centerOfMass[0]+2,1):
+	for x in xrange(centerOfMass[1]-1,centerOfMass[1]+2,1):
+		shapeDetector.test.originalImg[y,x] = [0,0,255]
 
 Gx,Gy = applyDGMasks(shapeDetector.test.imgCopy,height,width,mask,shapeDetector.test.border)
 
@@ -97,6 +106,8 @@ print "Gy: "
 print Gy
 
 print "x_min: ",x_min," centerOfMass: ",centerOfMass," radio: ",radio
+
+votes = dict()
 
 for i in range(len(shapeDetector.test.border)):
 	p = shapeDetector.test.border[i]
@@ -109,7 +120,64 @@ for i in range(len(shapeDetector.test.border)):
 	yc = int(round(p[0] - radio * sinTheta))
 	
 	if xc >= 0 and xc < x_max and yc >= 0 and yc < y_max:
-		shapeDetector.test.originalImg[yc,xc] = [0,0,255]
+		shapeDetector.test.originalImg[yc,xc] = [0,255,255]
+		vote = str(yc)+","+str(xc)
+		if vote not in votes:
+			votes[vote] = 1
+		else:
+			votes[vote] += 1
+bigger = ""
+for v in votes:
+	if bigger == "":
+		bigger = v
+	elif votes[v] > votes[bigger]:
+		bigger = v
+
+print bigger
+biggers = list()
+print "votes:  ",votes
+print "votes >= 4: "
+
+for v in votes:
+	if votes[v] >= 4:
+		print "votes[",v,"] :",votes[v]
+		biggers.append([int(v[0]+v[1]),int(v[3]+v[4])])
+
+yc_d = int(bigger[0]+bigger[1])
+xc_d = int(bigger[3]+bigger[4])
+"""
+for y in xrange(yc_d-1,yc_d+2,1):
+	for x in xrange(xc_d,xc_d+2,1):
+		shapeDetector.test.originalImg[y,x] = [0,0,0]
+"""
+for p in biggers:
+	shapeDetector.test.originalImg[p[0],p[1]] = [0,0,0]
+
+
+sum_x = 0
+sum_y = 0
+
+for e in biggers:
+    sum_x += e[1]
+    sum_y += e[0]
+
+lenght = len(biggers)
+center_c_d = [int(round(sum_y/lenght)),int(round(sum_x/lenght))]
+
+for y in xrange(center_c_d[0]-1,center_c_d[0]+2,1):
+	for x in xrange(center_c_d[1],center_c_d[1]+2,1):
+		shapeDetector.test.originalImg[y,x] = [0,0,0]
+
+print "borde 0: ",shapeDetector.test.border[0]
+"""
+for pixel in shapeDetector.test.border:
+	shapeDetector.test.originalImg[pixel[0],pixel[1]] = [0,0,255]
+"""
+newRadio = center_c_d[1] - x_min
+for y in range(1,height-1,1):
+	for x in range(1,width-1,1):
+		if circleEquation(x,center_c_d[1],y,center_c_d[0],newRadio):
+			shapeDetector.test.originalImg[y,x] = [0,140,255]
 
 
 cv2.imwrite("result_Circle.png",shapeDetector.test.originalImg)
