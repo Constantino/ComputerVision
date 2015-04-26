ead6b18b97a458e93e4fa037ff69de63c66c0704
constantino <constantinomora@gmail.com>
2015-04-26 14:39:27 -0500

    Dividing by methods to get the contours

--- MeasurementSystem/MeasurementSystem.py
+++ MeasurementSystem/MeasurementSystem.py
@@ -4,14 +4,28 @@ from matplotlib import pyplot as plt
 from random import choice,randint
 import math
 
-imgPath = "test004.jpg"
+imgPath = "test/test004.jpg"
 img = cv2.imread(imgPath,0)
 imgCopy = cv2.imread(imgPath)
 
-# Otsu's thresholding after Gaussian filtering                                                                                                  
-blur = cv2.GaussianBlur(img,(5,5),0)
-ret3,th3 = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+def filter_image(img):
+    # Otsu's thresholding after Gaussian filtering                                                                                             
+    blur = cv2.GaussianBlur(img,(5,5),0)
+    ret,th = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    return ret,th
+    
+def get_contours(th):
+    contours, hierarchy = cv2.findContours(th,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    return contours
 
-contours, hierarchy = cv2.findContours(th3,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+def main():
+    ret, th = filter_image(img)
+    contours = get_contours(th)
 
-print "len(contours): ",len(contours)
+    print "len(contours): ",len(contours)
+
+main()
+
+    
+            
+            
