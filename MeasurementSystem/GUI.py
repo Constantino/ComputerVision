9e969a9f81973b1e1cf4234abd197a0546cb7943
constantino <constantinomora@gmail.com>
2015-05-03 17:54:58 -0500

    Added validations for messages

--- MeasurementSystem/GUI.py
+++ MeasurementSystem/GUI.py
@@ -9,28 +9,50 @@ image = pygame.transform.scale(image,(400,600))
 imagerect = image.get_rect()
 width, height = imagerect.size
 
-size = ((width+150),height)
+panel = 90
+topPanel = 40
+
+size = ((width+panel),height)
 screen = pygame.display.set_mode(size)
 
 pygame.display.set_caption("Measurement System")
 font = pygame.font.Font(None, 20)
-btnFindObjects = font.render('Find Shapes',1,(0,0,250)) 
-btnSelectReference = font.render('Select reference',1,(0,255,0))
 
+btnFindObjects = font.render('Find Shapes',1,(0,0,250)) 
+btnMeasure = font.render('Measure',1,(0,255,0))
+found = False
+message = font.render('',1,(255,0,0))
+                
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
         
         if event.type == pygame.MOUSEBUTTONDOWN:
             cordx, cordy  = pygame.mouse.get_pos() 
-            if 0 < cordx < 100 and 0 < cordy < 25:
+            if 0 < cordx < panel and topPanel < cordy < 25+topPanel:
                 print "btnFindObjects"
-            if 0 < cordx < 100 and 35 < cordy < 55:
+                print "Select an object as reference"
+                message = font.render('Select an object as reference',1,(255,0,0))
+                found = True
+            if 0 < cordx < panel and (35+topPanel) < cordy < (55+topPanel):
                 print "btnSelectReference"
+
+            if panel < cordx < (width+panel) and topPanel < cordy < (height+topPanel):
+                if found:
+                    print "Checking if it is an object"
+                    message = font.render('Checking if it is an object',1,(255,0,0))
+                
+                else:
+                    print "You must find shapes first"
+                    message = font.render('You must find shapes first',1,(255,0,0))
+                
     
+
+
     screen.fill((0,0,0))
-    screen.blit(image,(150,0))
-    screen.blit(btnFindObjects,(0,10))
-    screen.blit(btnSelectReference,(0,40))
+    screen.blit(image,(panel,topPanel))
+    screen.blit(message,(200,15))
+    screen.blit(btnFindObjects,(0,topPanel))
+    screen.blit(btnMeasure,(0,topPanel*2))
     
     pygame.display.flip()
